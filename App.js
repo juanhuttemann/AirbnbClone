63b62011643d7bd2744fb2fabb25a9872fc99fdd
Juan Huttemann <juanfhuttemann@gmail.com>
2021-08-24 10:03:59 -0400

    add SearchResults component

--- App.js
+++ App.js
@@ -4,11 +4,12 @@ import {Text, View} from 'react-native';
 
 import Home from './src/screens/Home';
 import Post from './src/components/Post';
+import SearchResultsScreen from './src/screens/SearchResults';
 
 const App = () => {
   return (
     <View>
-      <Post />
+      <SearchResultsScreen />
     </View>
   );
 };
--- src/components/Post/index.js
+++ src/components/Post/index.js
@@ -2,30 +2,31 @@ import React from 'react';
 import {View, Text, Image} from 'react-native';
 import styles from './styles';
 
-const Post = () => {
+const Post = ({post}) => {
+  const {bed, bedrooms, type, title, oldPrice, newPrice, totalPrice, image} =
+    post;
   return (
     <View style={styles.container}>
       <Image
         style={styles.image}
         source={{
-          uri: 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/images/1.jpg',
+          uri: image,
         }}
       />
-      <Text style={styles.bedrooms}>1 bed 1 bedroom</Text>
+      <Text style={styles.bedrooms}>
+        {bed} bed {bedrooms} bedroom
+      </Text>
 
       <Text style={styles.description} numberOfLines={2}>
-        Ex sint quis fugiat nostrud deserunt voluptate est amet tempor dolore.
-        Laboris dolore ullamco ad laborum excepteur nostrud qui adipisicing
-        minim officia ea et. Velit incididunt exercitation mollit consequat do
-        fugiat sunt sint cupidatat est dolor.
+        {type}. {title}
       </Text>
 
       <Text style={styles.prices}>
-        <Text style={styles.oldPrice}>$36</Text>
-        <Text style={styles.price}> $30</Text> / night
+        <Text style={styles.oldPrice}>${oldPrice}</Text>
+        <Text style={styles.price}> ${newPrice}</Text> / night
       </Text>
 
-      <Text style={styles.totalPrice}>$230 total</Text>
+      <Text style={styles.totalPrice}>${totalPrice} total</Text>
     </View>
   );
 };
