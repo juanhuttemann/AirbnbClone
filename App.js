da30d2a12962edfbf1c0b36683215a15815ad0de
Juan Huttemann <juanfhuttemann@gmail.com>
2021-08-25 09:07:08 -0400

    add Guests screen

--- App.js
+++ App.js
@@ -6,11 +6,12 @@ import Home from './src/screens/Home';
 import Post from './src/components/Post';
 import SearchResultsScreen from './src/screens/SearchResults';
 import DestinationSearch from './src/screens/DestinationSearch';
+import GuestsScreen from './src/screens/Guests';
 
 const App = () => {
   return (
     <View>
-      <DestinationSearch />
+      <GuestsScreen />
     </View>
   );
 };
